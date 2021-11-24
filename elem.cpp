451f8a92488d60b6e335ca12a9c8591e73d0bbf6
esthera1017 <75809217+esthera1017@users.noreply.github.com>
2021-11-24 01:45:58 -0500

    Added setters

--- elem.cpp
+++ elem.cpp
@@ -3,3 +3,13 @@
 elem::elem(int width, int length, int xPos, int yPos) : width {width}, length {length}, xPos {xPos}, yPos {yPos}
 {}
 
+elem::setPos(int newX)
+{
+  xPos = newX;
+}
+
+elem::setYPos(int newY)
+{
+  yPos = newY;
+}
+
